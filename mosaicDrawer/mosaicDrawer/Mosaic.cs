b6952f710ce62ce3e8c3b9fd1802c59b7a282810
Nordic-Coding <tobiasforslund@hotmail.com>
2016-08-13 17:31:20 +0200

    Added old method for rendering quads on a plane, not finished

--- mosaicDrawer/mosaicDrawer/Mosaic.cs
+++ mosaicDrawer/mosaicDrawer/Mosaic.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenTK;
 using OpenTK.Graphics;
+//using OpenTK.Graphics.OpenGL4;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
 
 namespace mosaicDrawer
 {
@@ -22,13 +25,72 @@ namespace mosaicDrawer
             this.lx = lx;
             this.ly = ly;
 
-            mainWindow = new GameWindow(lx,ly,GraphicsMode.Default,"MosaicDrawer " + lx.ToString()+"x" + ly.ToString() + " Window");
             RGB = new byte[3 * px * py];
+
+            //runLoop(mainWindow);
         }
 
-        void loop()
+        public void runLoop()
         {
+            mainWindow = new GameWindow(lx, ly, GraphicsMode.Default, "MosaicDrawer " + lx.ToString() + "x" + ly.ToString() + " Window");
 
+            using (mainWindow)
+            {
+                //Enable vsync as standard.
+                mainWindow.Load += (sender, e) =>
+                {
+                    // setup settings, load textures, sounds
+                    mainWindow.VSync = VSyncMode.On;
+                };
+
+                //Resize handler
+                mainWindow.Resize += (sender, e) =>
+                {
+                    GL.Viewport(0, 0, mainWindow.Width, mainWindow.Height);
+                };
+
+                //The rendercalls, here the map should call its renderfunction.
+                mainWindow.RenderFrame += (sender, e) =>
+                {
+                    // render graphics
+
+                    GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+                    //Draw the mosaic
+                    GL.Begin(BeginMode.Quads);
+                    for (int i = 0; i < px; i++)
+                    {
+                        for(int j = 0; j < py; j++)
+                        {
+                            GL.Color3(RGB[3 * px * i + j * 3], RGB[3 * px * i + j * 3 + 1], RGB[3 * px * i + j * 3 + 2]);
+                            GL.Vertex2((double)((lx / px) * i)/(double)lx, (double)((ly / py) * i) / (double)ly);
+                            GL.Vertex2((double)((lx / px) * i-lx/(px)) / (double)lx, (double)((ly / py) * i) / (double)ly);
+                            GL.Vertex2((double)((lx / px) * i - lx/px) / (double)lx, (double)((ly / py) * i-ly/py) / (double)ly);
+                            GL.Vertex2((double)((lx / px) * i) / (double)lx, (double)((ly / py) * i - ly / py) / (double)ly);
+                        }
+                    }
+                    GL.End();
+
+                    mainWindow.SwapBuffers();
+                };
+
+                mainWindow.UpdateFrame += (sender, e) =>
+                {
+
+                    // add mainWindow logic, input handling here.
+                    if (mainWindow.Keyboard[Key.Escape])
+                    {
+                        mainWindow.Exit();
+                    }
+                };
+
+                // Run the mainWindow at 144 updates per second
+                mainWindow.Run(144.0);
+            }
+        }
+
+        public void updateMosaic(byte[] RGB)
+        {
+            this.RGB = RGB;
         }
     }
 }
--- mosaicDrawer/mosaicDrawer/Program.cs
+++ mosaicDrawer/mosaicDrawer/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace mosaicDrawer
@@ -11,7 +12,26 @@ namespace mosaicDrawer
         static void Main(string[] args)
         {
             //Just for testing the mosaic, will be removed later on.
+            Mosaic mosaic = new Mosaic(10,10,500,500);
+            Thread mosaicThread = new Thread(()=>mosaic.runLoop());
+            byte[] rgb = new byte[10*10*3];
+            mosaicThread.Start();
+            Random random = new Random();
+            
+            while (1 > 0)
+            {
+                //Console.WriteLine("AAAAAAAAAAAAAAAA");
+                gibBytes(ref random,ref rgb);
+                mosaic.updateMosaic(rgb);
+                Thread.Sleep(20);
+            }
+            //Thread workerThread = new Thread(mosaic.);
+        }
 
+        public static void gibBytes(ref Random ran, ref byte[] ret)
+        {
+            //Generate a random byte sequence.
+            ran.NextBytes(ret);
         }
     }
 }
