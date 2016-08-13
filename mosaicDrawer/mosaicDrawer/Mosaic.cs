5391b003fc8941af295bed61d3b22817307cf904
Nordic-Coding <tobiasforslund@hotmail.com>
2016-08-13 20:19:28 +0200

    Finalized renderer, now uses texture methods to render a 2D-Mosaic.

--- mosaicDrawer/mosaicDrawer/Mosaic.cs
+++ mosaicDrawer/mosaicDrawer/Mosaic.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using OpenTK;
 using OpenTK.Graphics;
@@ -15,9 +16,9 @@ namespace mosaicDrawer
     public class Mosaic
     {
         GameWindow mainWindow;
-        int px, py, lx, ly;
+        int px, py, lx, ly, texId;
         int counter;
-        byte[] RGB;
+        //byte[] RGB;
         public bool drawing;
 
         public Mosaic(int px, int py, int lx, int ly)
@@ -27,13 +28,14 @@ namespace mosaicDrawer
             this.lx = lx;
             this.ly = ly;
             counter = 0;
-            RGB = new byte[3 * px * py];
+            //RGB = new byte[3 * px * py];
             drawing = false;
-            //runLoop(mainWindow); sddsfsdf
+
         }
 
-        public void runLoop()
+        public void runLoop(byte[] RGB)
         {
+
             mainWindow = new GameWindow(lx, ly, GraphicsMode.Default, "MosaicDrawer " + lx.ToString() + "x" + ly.ToString() + " Window");
 
             using (mainWindow)
@@ -44,7 +46,11 @@ namespace mosaicDrawer
                     // setup settings, load textures, sounds
                     //mainWindow.VSync = VSyncMode.On;
                     GL.Enable(EnableCap.Texture2D);
-
+                    //GL.Enable(EnableCap.Blend);
+                    texId = GL.GenTexture();
+                    GL.BindTexture(TextureTarget.Texture2D, texId);
+                    GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Nearest);
+                    GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Nearest);
                 };
 
                 //Resize handler
@@ -59,20 +65,18 @@ namespace mosaicDrawer
                     // render graphics
                     drawing = true;
                     GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-                    GL.BindTexture(TextureTarget.Texture2D, );
+                    GL.MatrixMode(MatrixMode.Modelview);
+                    GL.LoadIdentity();
+                    GL.BindTexture(TextureTarget.Texture2D, texId);
+                    GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgb, px, py, 0,
+                        OpenTK.Graphics.OpenGL.PixelFormat.Rgb, PixelType.Byte, RGB);
                     //Draw the mosaic
                     GL.Begin(PrimitiveType.Quads);
-                    for (int i = 0; i < px; i++)
-                    {
-                        for(int j = 0; j < py; j++)
-                        {
-                            GL.Color3(RGB[3 * px * i + j * 3], RGB[3 * px * i + j * 3 + 1], RGB[3 * px * i + j * 3 + 2]);
-                            GL.Vertex2((double)((lx / px) * i)/(double)lx*2f-1f, (double)((ly / py) * j) / (double)ly * 2f - 1f);
-                            GL.Vertex2((double)((lx / px) * i-lx/(px)) / (double)lx * 2f + 1f, (double)((ly / py) * j) / (double)ly * 2f - 1f);
-                            GL.Vertex2((double)((lx / px) * i - lx/px) / (double)lx * 2f + 1f, (double)((ly / py) * j-ly/py) / (double)ly * 2f + 1f);
-                            GL.Vertex2((double)((lx / px) * i) / (double)lx * 2f - 1f, (double)((ly / py) * j - ly / py) / (double)ly * 2f + 1f);
-                        }
-                    }
+                        GL.Color3(1f,1f,1f);
+                        GL.TexCoord2(0.0f, 0.0f); GL.Vertex2(-1f, -1f);
+                        GL.TexCoord2(1.0f, 0.0f); GL.Vertex2(1f, -1f);
+                        GL.TexCoord2(1.0f, 1.0f); GL.Vertex2(1f, 1f);
+                        GL.TexCoord2(0.0f, 1.0f); GL.Vertex2(-1f, 1f);
                     GL.End();
                     counter++;
                     mainWindow.Title = "MosaicDrawer " + lx.ToString() + "x" + ly.ToString() + " Window" + "(" + counter.ToString() + ")";
@@ -91,17 +95,15 @@ namespace mosaicDrawer
                 };
 
                 // Run the mainWindow at 144 updates per second
-                mainWindow.Run(60.0);
+                mainWindow.Run(144.0);
             }
         }
-
+        /*
         public void updateMosaic(ref byte[] RGB)
         {
             //Bind the referenced RGB to the texture.
             this.RGB = RGB;
-
-
-        }
+        }*/
 
     }
 }
--- mosaicDrawer/mosaicDrawer/Program.cs
+++ mosaicDrawer/mosaicDrawer/Program.cs
@@ -12,19 +12,19 @@ namespace mosaicDrawer
         static void Main(string[] args)
         {
             //Just for testing the mosaic, will be removed later on.
-            int px = 100, py = 100;
+            int px = 500, py = 500;
             Mosaic mosaic = new Mosaic(px,py,500,500);
-            Thread mosaicThread = new Thread(()=>mosaic.runLoop());
             byte[] rgb = new byte[px*py*3];
+            Thread mosaicThread = new Thread(() => mosaic.runLoop(rgb));
+            //mosaic.updateMosaic(ref rgb);
             mosaicThread.Start();
             Random random = new Random();
-            
             while (1 > 0)
             {
                 //Console.WriteLine("AAAAAAAAAAAAAAAA");
                 gibBytes(ref random,ref rgb);
-                mosaic.updateMosaic(ref rgb);
-                Thread.Sleep(25);
+                //mosaic.updateMosaic(ref rgb);
+                Thread.Sleep(10);
             }
             //Thread workerThread = new Thread(mosaic.);
         }
@@ -32,9 +32,10 @@ namespace mosaicDrawer
         public static void gibBytes(ref Random ran, ref byte[] ret)
         {
             //Generate a random byte sequence.
-            for(int i = 0; i < ret.Length; i++)
+            ran.NextBytes(ret);
+            /*for(int i = 0; i < ret.Length; i++)
             {
-                if(ret[i] > 200)
+                /*if(ret[i] > 200)
                 {
                     ret[i] = (byte)(ret[i] / 10);
                 }
@@ -43,7 +44,8 @@ namespace mosaicDrawer
                     ret[i] = (byte)(ret[i] + 1 + (byte)ran.Next()/10);
                 }
                 
-            }
+            }*/
+            Console.WriteLine("y");
         }
     }
 }
